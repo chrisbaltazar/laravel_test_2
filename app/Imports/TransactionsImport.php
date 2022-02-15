dbbb91d34fc8c21fed313101d8294ff61ebb04a6
Christian <christian@netzstrategen.com>
2022-02-16 00:18:25 +0100

    Added docs.

--- app/Imports/TransactionsImport.php
+++ app/Imports/TransactionsImport.php
@@ -2,9 +2,7 @@
 
 namespace App\Imports;
 
-use App\Models\Transaction;
 use Maatwebsite\Excel\Concerns\ToArray;
-use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
 class TransactionsImport implements ToArray, WithHeadingRow
--- app/Services/TransactionsReader.php
+++ app/Services/TransactionsReader.php
@@ -5,8 +5,18 @@ namespace App\Services;
 interface TransactionsReader
 {
 
+    /**
+     * Returns the specific type/source for each reader.
+     *
+     * @return string
+     */
     public static function getType(): string;
 
+    /**
+     * Gathers and returns the array of transactions from the specific source.
+     *
+     * @return array
+     */
     public function retrieveTransactions(): array;
 
 }
